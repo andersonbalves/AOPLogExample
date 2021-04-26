f91cb2c08d265cff6450c33fb7f8ccf3e4ce16c5
andersonbalves <baratella.anderson@gmail.com>
2021-04-26 17:55:44 -0300

    Commit Inicial

---
+++ src/main/java/br/com/baratella/aoplogexample/AopLogExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.baratella.aoplogexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopLogExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopLogExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/baratella/aoplogexample/AopLogExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.baratella.aoplogexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopLogExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
